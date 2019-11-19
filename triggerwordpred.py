53e43416d2b14249e887a5b9dfb537a55b20b855
Jabor047 <gkkarobia@gmail.com>
2019-11-19 19:59:13 +0300

    Added a better and faster method for audio recording

--- triggerwordpred.py
+++ triggerwordpred.py
@@ -1,5 +1,7 @@
 import utilities
 import time
+import pyaudio
+import wave
 import numpy as np
 import soundfile as sf
 import sounddevice as sd
@@ -10,7 +12,7 @@ from tensorflow.keras.models import load_model
 
 def record():
     samplerate = 16000
-    duration = 10
+    duration = 10.5
     filename = 'trigger.wav'
     
     recording = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, blocking=True)
@@ -18,15 +20,48 @@ def record():
     sd.wait()
     print('Saving your command')
     sf.write(filename, recording, samplerate)
+    
+
+def take_input():
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    RECORD_SECONDS = 3
+    WAVE_OUTPUT_NAME = 'trigger.wav'
+    
+    p = pyaudio.PyAudio()
+    
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                    input=True, frames_per_buffer=CHUNK)
+    
+    print('\n\n\n************ Started Lisening ___________________')
+    frames = []
+    for i in range(int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    
+    print('************* Done Listening')
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    
+    wf = wave.open(WAVE_OUTPUT_NAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close
+    return WAVE_OUTPUT_NAME
 
 
-def preprocess_audio():
+def preprocess_audio(audio):
     padding = AudioSegment.silent(duration=10000)
-    segment = AudioSegment.from_wav('trigger.wav')[:10000]
+    segment = AudioSegment.from_wav(audio)[:10000]
     segment = padding.overlay(segment)
     
     segment = segment.set_frame_rate(44100)
-    segment.export('trigger.wav', format='wav')
+    segment.export(audio, format='wav')
 
 
 def detect_triggerword(audio, model):
@@ -56,14 +91,14 @@ def chime_on(audio, chime_file, predictions, threshold):
 
 
 def main():
-    model = load_model('tr_model.h5')
+    model = load_model('best_trigger_model_TM_30.h5')
     chime_file = 'chime.wav'
     print('Hello there, please record your command immediately after you see start')
     time.sleep(4)
-    print('Start')
-    record()
-    preprocess_audio()
-    audio = 'trigger.wav'
+    # print('Start')
+    # record()
+    audio = take_input()
+    preprocess_audio(audio)
     preds = detect_triggerword(audio, model)
     chime_threshold = 0.5
     chime_on(audio, chime_file, preds, chime_threshold)
