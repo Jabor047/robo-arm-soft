88c807530534f8cf23e90a22e7c33d47cdeb1089
Jabor047 <gkkarobia@gmail.com>
2019-11-27 06:25:30 +0300

    added a shell on how to run the commands prediction script

--- triggerwordpred.py
+++ triggerwordpred.py
@@ -2,6 +2,7 @@ from triggerword import utilities
 import time
 import pyaudio
 import wave
+import subprocess
 import numpy as np
 import soundfile as sf
 import sounddevice as sd
@@ -9,18 +10,21 @@ import matplotlib.pyplot as plt
 from pydub import AudioSegment
 from tensorflow.keras.models import load_model
 
+python_path = r'C:\Python3\python.exe'
+commandspred_path = 'E:\\projects\\deep_learning\\dataset\\5th\\code\\commandspred.py'
+
 
 def record():
     samplerate = 16000
     duration = 10.5
     filename = 'trigger.wav'
-    
-    recording = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, blocking=True)
+    recording = sd.rec(int(samplerate * duration), samplerate=samplerate,
+                       channels=1, blocking=True)
     print(recording)
     sd.wait()
     print('Saving your command')
     sf.write(filename, recording, samplerate)
-    
+
 
 def take_input():
     CHUNK = 1024
@@ -29,23 +33,22 @@ def take_input():
     RATE = 44100
     RECORD_SECONDS = 3
     WAVE_OUTPUT_NAME = 'trigger.wav'
-    
+
     p = pyaudio.PyAudio()
-    
+
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
                     input=True, frames_per_buffer=CHUNK)
-    
+
     print('\n\n\n************ Started Lisening ___________________')
     frames = []
     for i in range(int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
         frames.append(data)
-    
+
     print('************* Done Listening')
     stream.stop_stream()
     stream.close()
     p.terminate()
-    
     wf = wave.open(WAVE_OUTPUT_NAME, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
@@ -59,7 +62,7 @@ def preprocess_audio(audio):
     padding = AudioSegment.silent(duration=10000)
     segment = AudioSegment.from_wav(audio)[:10000]
     segment = padding.overlay(segment)
-    
+
     segment = segment.set_frame_rate(44100)
     segment.export(audio, format='wav')
 
@@ -69,7 +72,7 @@ def detect_triggerword(audio, model):
     x = x.swapaxes(0, 1)
     x = np.expand_dims(x, axis=0)
     predictions = model.predict(x)
-    
+
     plt.subplot(2, 1, 2)
     plt.plot(predictions[0, :, 0])
     plt.ylabel('probability')
@@ -85,7 +88,9 @@ def chime_on(audio, chime_file, predictions, threshold):
     for i in range(Ty):
         consecutives_timesteps += 1
         if predictions[0, i, 0] > threshold and consecutives_timesteps > 75:
-            audio_clip = audio_clip.overlay(chime, position=((i / Ty) * audio_clip.duration_seconds) * 1000)
+            # subprocess.Popen([python_path, commandspred_path])
+            audio_clip = audio_clip.overlay(chime, position=(
+                (i / Ty) * audio_clip.duration_seconds) * 1000)
             consecutives_timesteps = 0
     audio_clip.export('Chime_ouput.wav', format='wav')
 
@@ -93,7 +98,8 @@ def chime_on(audio, chime_file, predictions, threshold):
 def main():
     model = load_model('triggerword/best_trigger_model_TM_100.h5')
     chime_file = 'chime.wav'
-    print('Hello there, please record your command immediately after you see start')
+    print('Hello there, please record your'
+          'command immediately after you see start')
     time.sleep(4)
     # print('Start')
     # record()
@@ -102,7 +108,7 @@ def main():
     preds = detect_triggerword(audio, model)
     chime_threshold = 0.5
     chime_on(audio, chime_file, preds, chime_threshold)
-    
+
 
 if __name__ == '__main__':
     main()
